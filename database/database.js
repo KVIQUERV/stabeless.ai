d82b46b7aea504321a7486803bcb62741a9bb642
Kaique Silva <68kaiquesilva@gmail.com>
2024-10-21 21:17:11 -0300

    init: hello, world

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const sequelize = require("sequelize");
+
+const connection = new sequelize("db_stabeless.ai", "root", "983503401", {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+module.exports = connection;
---
+++ main.js
@@ -0,0 +1,32 @@
+/* -=|> Libraries/Internal Files */
+const connection = require("./database/database.js");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+/* -=|> Express Instance */
+const app = express();
+
+app.set("view engine", "ejs"); /* -=|> View Engine */
+app.use(express.static("public")); /* -=|> Static */
+app.use(bodyParser.urlencoded({ extended: false })); /* -=|> Body-Parser */
+app.use(bodyParser.json()); /* -=|> Body-Parser */
+
+/* -=|> Database Connection */
+connection
+    .authenticate()
+    .then(() => {
+        console.log("[ON] Database successfully connected!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+/* -=|> Routes */
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+/* -=|> Server Initialization */
+app.listen(8080, () => {
+    console.log("[ON] Server successfully initialized!");
+});
